926e40e2c2d00e0428ccd24731b4df68708ab89b
Evelyn Kosma <ezranaja@gmail.com>
2021-02-28 20:54:13 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,7 +2,7 @@ def my_collect(array)
   ret = []
   counter = 0 
   while counter < array.length  
-    ret[counter] = yield(ret[counter]) 
+    ret[counter] = yield(array[counter]) 
     counter += 1
   end
   return ret
